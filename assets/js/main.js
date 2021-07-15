1e238158dfdb3950b75d2510b76610f690afbcb3
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-07-14 19:20:29 -0500

    working on js stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 console.log("Is this working");
 
 
-let billAmount = 1;
-let numberOfGuests = 4
+let billAmount = 100;
+let numberOfGuests = 4;
+let gratuity = 15;
 
 
+
+//! Functions
 // get bill total from form input
 function enterBill() {
     let billAmount = document.getElementById("billAmountForm").value;
@@ -12,38 +15,47 @@ function enterBill() {
     //console.log("bill amount: " + billAmount);
 }
 
-console.log("bill amount: " + billAmount);
+function setGratuityPercent() {
+    return (gratuity / 100)
+}
 
-// function setGratuityPercent() {
-//    let percent = 0.20;
-// }
+function gratuityPercent() {
+    return (setGratuityPercent() * 100);
+}
+
 
 // figure out gratuity - 20%
 function calculateGratuity() {
-    return (billAmount * 0.2);
+    return (billAmount * setGratuityPercent());
 }
 
-console.log("Gratuity is: " + calculateGratuity());
 
 function totalWithGrat(amount) {
     return calculateGratuity() + amount;
 }
 
 
+//! logging
+
+console.log("bill amount: " + billAmount);
+console.log("Thar be " + numberOfGuests + " guests");
+console.log("Gratuity(%) is: " + gratuityPercent() + "%");
+console.log("Gratuity(math stuff) is: " + setGratuityPercent());
+console.log("Gratuity($) is: $" + calculateGratuity());
 console.log("Bill amount: " + totalWithGrat());
 console.log("Your total including gratuity is:" + `totalWithGrat(billAmount)`);
 
 
 
-// add stuff to the frontend
+//! add stuff to the frontend
 
 // Bill 
 const billTotal = document.getElementById("bill-amount");
 billTotal.innerHTML = billAmount;
 
 // gratuity percent %%% 
-// const gratuityPrecent = document.getElementById("gratuity-percent");
-// gratuityPrecent.innerHTML = setGratuityPercent();
+//const gratuityPrecent = document.getElementById("gratuity-percent");
+//gratuityPrecent.innerHTML = setGratuityPercent();
 
 // gratuity amount $$$ 
 const gratuityAmount = document.getElementById("gratuity-amount");
